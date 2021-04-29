e98af45d1a76249ed4bbbda7eb6d0c024e491a8d
Bartosz Biegajło <bartosz.biegajlo@gmail.com>
2021-04-29 11:59:26 +0200

    make PhoneViewModel

---
+++ app/src/main/java/com/example/phonebase/PhoneViewModel.java
@@ -0,0 +1,33 @@
+package com.example.phonebase;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+/*  Model task is to hold data that needs to be displayed in the activity.
+    Models can last longer than activities, thus they allow to retrieve data after activity restart.
+ */
+public class PhoneViewModel extends AndroidViewModel {
+
+    private final PhoneRepository mRepository;
+    private final LiveData<List<Phone>> mAllPhones;
+
+    public PhoneViewModel(@NonNull Application application) {
+        super(application);
+
+        mRepository = new PhoneRepository(application);
+        mAllPhones = mRepository.getAllPhones();
+    }
+
+    LiveData<List<Phone>> getAllPhones() {
+        return mAllPhones;
+    }
+
+    public void deleteAll() {
+        mRepository.deleteAll();
+    }
+}
