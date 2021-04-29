0c584f3b3e674294b46bdf17314b9934673d6b7f
Bartosz Biegajło <bartosz.biegajlo@gmail.com>
2021-04-29 11:51:25 +0200

    make PhoneRepository

---
+++ app/src/main/java/com/example/phonebase/PhoneRepository.java
@@ -0,0 +1,33 @@
+package com.example.phonebase;
+
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+/*  Repository is a single data source for the application. It Implements logic that decides where
+    the data should be retrieved from. It uses RoomDatabase class and DAO.
+ */
+public class PhoneRepository {
+
+    private PhoneDao mPhoneDao;
+    private LiveData<List<Phone>> mAllPhones;
+
+    PhoneRepository(Application application) {
+
+        PhoneRoomDatabase phoneRoomDatabase = PhoneRoomDatabase.getDatabase(application);
+        mPhoneDao = phoneRoomDatabase.phoneDao();
+        mAllPhones = mPhoneDao.getAlphabetizedElements();
+    }
+
+    LiveData<List<Phone>> getAllPhones() {
+        return mAllPhones;
+    }
+
+    void deleteAll() {
+        PhoneRoomDatabase.databaseWriteExecutor.execute(() -> {
+            mPhoneDao.deleteAll();
+        });
+    }
+}
