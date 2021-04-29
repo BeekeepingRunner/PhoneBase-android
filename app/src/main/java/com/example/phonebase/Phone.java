9fef2b11b86d9f7d61affa9d445a051201a1b896
Bartosz Biegajło <bartosz.biegajlo@gmail.com>
2021-04-29 10:54:05 +0200

    make Phone (entity) class

--- app/src/main/java/com/example/phonebase/Phone.java
+++ app/src/main/java/com/example/phonebase/Phone.java
@@ -1,6 +1,83 @@
 package com.example.phonebase;
 
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "phone")
 public class Phone {
 
-    
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    private long id;
+
+    @NonNull
+    @ColumnInfo(name = "manufacturer")
+    private String manufacturer;
+
+    @NonNull
+    @ColumnInfo(name = "model")
+    private String model;
+
+    @NonNull
+    @ColumnInfo(name = "androidVersion")
+    private String androidVersion;
+
+    @NonNull
+    @ColumnInfo(name = "site")
+    private String site;
+
+    public Phone(long id, @NonNull String manufacturer, @NonNull String model,
+                 @NonNull String androidVersion, @NonNull String site) {
+        this.id = id;
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.androidVersion = androidVersion;
+        this.site = site;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @NonNull
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(@NonNull String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    @NonNull
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(@NonNull String model) {
+        this.model = model;
+    }
+
+    @NonNull
+    public String getAndroidVersion() {
+        return androidVersion;
+    }
+
+    public void setAndroidVersion(@NonNull String androidVersion) {
+        this.androidVersion = androidVersion;
+    }
+
+    @NonNull
+    public String getSite() {
+        return site;
+    }
+
+    public void setSite(@NonNull String site) {
+        this.site = site;
+    }
 }
