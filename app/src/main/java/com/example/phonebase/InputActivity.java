5f459b092596b3621f4d348b3a963b4343ed8759
Bartosz Biegajło <biegbart.z@gmail.com>
2021-05-07 19:24:21 +0200

    changed buttons' ids in data input layout

--- app/src/main/java/com/example/phonebase/InputActivity.java
+++ app/src/main/java/com/example/phonebase/InputActivity.java
@@ -3,6 +3,7 @@ package com.example.phonebase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class InputActivity extends AppCompatActivity {
 
