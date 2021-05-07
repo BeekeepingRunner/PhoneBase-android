0325e28f30d77d9025788279caa1170dcc9cbc5e
Bartosz Biegajło <biegbart.z@gmail.com>
2021-05-07 19:27:52 +0200

    cancel button causes return to the main activ., data is lost

--- app/src/main/java/com/example/phonebase/InputActivity.java
+++ app/src/main/java/com/example/phonebase/InputActivity.java
@@ -3,13 +3,21 @@ package com.example.phonebase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class InputActivity extends AppCompatActivity {
+
+    Button cancelButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
+
+        cancelButton = findViewById(R.id.cancel_button);
+        cancelButton.setOnClickListener((View v) -> {
+            finish();
+        });
     }
 }
