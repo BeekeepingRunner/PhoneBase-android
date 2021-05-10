e22c751d8556844e7abaf716ebca5bb0217f67d4
Bartosz Biegajło <biegbart.z@gmail.com>
2021-05-10 17:31:34 +0200

    Added null input check

--- app/src/main/java/com/example/phonebase/InputActivity.java
+++ app/src/main/java/com/example/phonebase/InputActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.TreeMap;
 
@@ -51,15 +53,45 @@ public class InputActivity extends AppCompatActivity {
     // After clicking save button, entered data is passed to the main activity
     void setSaveButton() {
         saveButton = findViewById(R.id.save_button);
+
         saveButton.setOnClickListener((View v) -> {
 
-            Intent intent = new Intent();
-            intent.putExtra(MainActivity.PHONE_MANUFACTURER_INPUT, manufacturerEditText.getText().toString());
-            intent.putExtra(MainActivity.PHONE_MODEL_INPUT, modelEditText.getText().toString());
-            intent.putExtra(MainActivity.PHONE_ANDROID_VERSION_INPUT, androidVersionEditText.getText().toString());
-            intent.putExtra(MainActivity.PHONE_WEBSITE_INPUT, websiteEditText.getText().toString());
-            setResult(RESULT_OK, intent);
-            finish();
+            if (isInputComplete()) {
+                Intent intent = new Intent();
+                intent.putExtra(MainActivity.PHONE_MANUFACTURER_INPUT, manufacturerEditText.getText().toString());
+                intent.putExtra(MainActivity.PHONE_MODEL_INPUT, modelEditText.getText().toString());
+                intent.putExtra(MainActivity.PHONE_ANDROID_VERSION_INPUT, androidVersionEditText.getText().toString());
+                intent.putExtra(MainActivity.PHONE_WEBSITE_INPUT, websiteEditText.getText().toString());
+                setResult(RESULT_OK, intent);
+                finish();
+            }
         });
+    }
+
+    boolean isInputComplete() {
+
+        String manufacturer = manufacturerEditText.getText().toString().trim();
+        String model = modelEditText.getText().toString().trim();
+        String androidVersion = androidVersionEditText.getText().toString().trim();
+        String website = websiteEditText.getText().toString().trim();
+
+        if (TextUtils.isEmpty(manufacturer)) {
+            manufacturerEditText.setError("Manufacturer name cannot be empty");
+            return false;
+        }
+        if (TextUtils.isEmpty(model)) {
+            modelEditText.setError("Model name cannot be empty");
+            return false;
+        }
+        if (TextUtils.isEmpty(androidVersion)) {
+            androidVersionEditText.setError("Android version cannot be empty");
+            return false;
+        }
+        if (TextUtils.isEmpty(website)) {
+            websiteEditText.setError("Website field cannot be empty");
+            return false;
+        }
+
+        return true;
     }
 }
